dacc609eeeaaa6575a450670784b7d958678432c
Mohammad Ashraful Kabir <maksumon007@gmail.com>
2022-01-11 23:20:46 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Mohammad Ashraful Kabir on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
