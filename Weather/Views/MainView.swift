571e3f460a3176ab7799877777f201576059bd77
Mohammad Ashraful Kabir <maksumon007@gmail.com>
2022-01-17 12:59:14 +0800

    Implemented LoadingView

--- Weather/Views/MainView.swift
+++ Weather/Views/MainView.swift
@@ -18,97 +18,90 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    List {
-                        if (viewModel.weather != nil) {
-                            Section(header: Text("Today").font(.system(size: 14, weight: .bold, design: .default))) {
-                                CurrentWeatherConditionView(
-                                    currentWeather: (viewModel.weather?.current)!,
-                                    currentDay: (viewModel.weather?.forecast?.forecastday?.first?.day)!
-                                )
+            LoadingView(isShowing: $viewModel.isLoading) {
+                GeometryReader { geometry in
+                    ZStack(alignment: .leading) {
+                        List {
+                            if (viewModel.weather != nil) {
+                                Section(header: Text("Today").font(.system(size: 14, weight: .bold, design: .default))) {
+                                    CurrentWeatherConditionView(
+                                        currentWeather: (viewModel.weather?.current)!,
+                                        currentDay: (viewModel.weather?.forecast?.forecastday?.first?.day)!
+                                    )
+                                }
+                            }
+                            if (viewModel.weather != nil) {
+                                Section(header: Text("Hourly").font(.system(size: 14, weight: .bold, design: .default))) {
+                                    HourlyView(hours: (viewModel.weather?.forecast?.forecastday?.first?.hour)!)
+                                }
+                            }
+                            if (viewModel.weather != nil) {
+                                Section(header: HStack {
+                                    Text("Air Quality").font(.system(size: 14, weight: .bold, design: .default))
+                                    Spacer()
+                                    NavigationLink("Info", destination: AirQualityDetailsView())
+                                }) {
+                                    AirQualitySummaryView(currentWeather: (viewModel.weather?.current)!)
+                                }
+                            }
+                            if (viewModel.weather != nil) {
+                                Section(header: Text("Forecasts").font(.system(size: 14, weight: .bold, design: .default))) {
+                                    ForecastsView(forecast: (viewModel.weather?.forecast)!)
+                                }
                             }
                         }
-                        if (viewModel.weather != nil) {
-                            Section(header: Text("Hourly").font(.system(size: 14, weight: .bold, design: .default))) {
-                                HourlyView(hours: (viewModel.weather?.forecast?.forecastday?.first?.hour)!)
-                            }
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .offset(x: self.showMenu ? geometry.size.width/1.5 : 0)
+                        .disabled(self.showMenu ? true : false)
+                        .pullToRefresh(isShowing: $isPullToRefresh) {
+                            self.viewModel.fetchWeather(city: viewModel.currentCity)
+                            self.isPullToRefresh = false
                         }
-                        if (viewModel.weather != nil) {
-                            Section(header: HStack {
-                                Text("Air Quality").font(.system(size: 14, weight: .bold, design: .default))
-                                Spacer()
-                                NavigationLink("Info", destination: AirQualityDetailsView())
-                            }) {
-                                AirQualitySummaryView(currentWeather: (viewModel.weather?.current)!)
-                            }
+                        if self.showMenu {
+                            SideMenuView()
+                                .frame(width: geometry.size.width/1.5)
+                                .transition(.move(edge: .leading))
                         }
-                        if (viewModel.weather != nil) {
-                            Section(header: Text("Forecasts").font(.system(size: 14, weight: .bold, design: .default))) {
-                                ForecastsView(forecast: (viewModel.weather?.forecast)!)
-                            }
+                        NavigationLink(
+                            destination: LocationSearchView(),
+                            isActive: self.$pushLocationSearchView
+                        ) {
+                            EmptyView()
                         }
+                        .frame(width: 0, height: 0)
+                        .disabled(true)
                     }
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                    .offset(x: self.showMenu ? geometry.size.width/1.5 : 0)
-                    .disabled(self.showMenu ? true : false)
-                    .pullToRefresh(isShowing: $isPullToRefresh) {
-                        self.viewModel.fetchWeather(city: viewModel.currentCity)
-                        self.isPullToRefresh = false
-                    }
-                    if self.showMenu {
-                        SideMenuView()
-                            .frame(width: geometry.size.width/1.5)
-                            .transition(.move(edge: .leading))
-                    }
-                    if viewModel.isLoading {
-                        ProgressView()
-                            .scaleEffect(1.5, anchor: .center)
-                            .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                            .frame(width: geometry.size.width)
-                    }
-                    NavigationLink(
-                        destination: LocationSearchView(),
-                        isActive: self.$pushLocationSearchView
-                    ) {
-                        EmptyView()
-                    }
-                    .frame(width: 0, height: 0)
-                    .disabled(true)
                 }
-            }
-            .navigationBarTitle(
-                viewModel.weather != nil
-                    ? (viewModel.weather?.location?.name)!
-                    : "Weather")
-            .navigationBarItems(
-                leading: (
-                    Button(
-                        action: {
-                            withAnimation {
-                                self.showMenu.toggle()
+                .navigationBarTitle(
+                    viewModel.weather != nil
+                        ? (viewModel.weather?.location?.name)!
+                        : "Weather")
+                .navigationBarItems(
+                    leading: (
+                        Button(
+                            action: {
+                                withAnimation {
+                                    self.showMenu.toggle()
+                                }
                             }
+                        ) {
+                            Image(systemName: "line.horizontal.3")
+                                .imageScale(.large)
                         }
-                    ) {
-                        Image(systemName: "line.horizontal.3")
-                            .imageScale(.large)
-                    }
-                ),
-                trailing: (
-                    Button(
-                        action: {
-                            withAnimation {
-                                self.pushLocationSearchView = true
+                    ),
+                    trailing: (
+                        Button(
+                            action: {
+                                withAnimation {
+                                    self.pushLocationSearchView = true
+                                }
                             }
+                        ) {
+                            Image(systemName: "plus")
+                                .imageScale(.large)
                         }
-                    ) {
-                        Image(systemName: "plus")
-                            .imageScale(.large)
-                    }
+                    )
                 )
-            )
-            .onAppear {
-                viewModel.getCurrentLocationAndFetchWeather()
             }
         }
     }
